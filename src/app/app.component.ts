e074415788ebd5e7c9382962364447d3097419e3
Matthew Watson <Me@MatthewTheDeveloper.com>
2022-07-11 01:00:08 -0600

    - setup the project - added angular material - figured out a basic form field file selector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'file-selector';
+  
+  files: File[] = [];
+  
+  onFilesSelected(): void {
+    const inputNode: any = document.querySelector('#file');
+
+    this.files = Array.from(inputNode.files);
+
+    console.log(inputNode.files);
+  }
+
+  remove(file: File): void {
+    console.log(this.files);
+    const index = this.files.indexOf(file);
+
+    if (index >= 0) {
+      this.files.splice(index, 1);
+    }
+  }
 }
